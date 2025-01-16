f3312ecb86d26c88d884cc602998693cecac62d7
EudaldGM <eudaldguillen@gmail.com>
2025-01-16 15:06:44 +0100

    fix

--- go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/EudaldGM/thornlogin
-
-go 1.23.4
-
-require golang.org/x/crypto v0.32.0
--- user.go
+++ user.go
@@ -11,6 +11,12 @@ type password struct {
 	password string
 }
 
+func newPassword(pw string) password {
+	var p password
+	p.password = pw
+	return p
+}
+
 type saltedPwd struct {
 	saltedPwd []byte
 }
@@ -25,8 +31,8 @@ func (p password) saltPwd() saltedPwd {
 	return spwd
 }
 
-func (sp saltedPwd) verifyPassword(p password) bool {
-	err := bcrypt.CompareHashAndPassword([]byte(sp.saltedPwd), []byte(p.password))
+func (sp saltedPwd) verifyPassword(p string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(sp.saltedPwd), []byte(p))
 	if err != nil {
 		return false
 	} else {
